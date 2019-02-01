a7e7af65fe53100560d145d866b70878a0efd00f
Faizan <faizanmir009@gmail.com>
2019-02-01 17:53:29 +0530

    Start

---
+++ app/src/main/java/bookkeeper/com/bookkeeper/LoginActivity.java
@@ -0,0 +1,104 @@
+package bookkeeper.com.bookkeeper;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import me.anwarshahriar.calligrapher.Calligrapher;
+
+public class LoginActivity extends AppCompatActivity {
+    final int RC_SIGN_IN = 1;
+    private FirebaseAuth mAuth;
+    SharedPreferences sharedPreferences;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_login);
+        sharedPreferences = getSharedPreferences("loginPrefs" , MODE_PRIVATE);
+        if(sharedPreferences.getInt("login_h",9)==1)
+        {
+            startActivity(new Intent(LoginActivity.this, bookkeeper.com.bookkeeper.mainActivity.class));
+            finish();
+        }
+
+        Calligrapher calligrapher = new Calligrapher(this);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.webid)).requestEmail().build();
+        final GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        calligrapher.setFont(this, "fonts/Product Sans Bold.ttf", true);
+
+
+        CardView sign_in = findViewById(R.id.signin);
+        View.OnClickListener signin = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        };
+        sign_in.setOnClickListener(signin);
+    }
+
+    @Override
+    protected void onActivityResult ( int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
+        }
+    }
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent mainActivity = new Intent(LoginActivity.this, bookkeeper.com.bookkeeper.mainActivity.class);
+                            sharedPreferences.edit().putInt("login_h",1).apply();
+                            startActivity(mainActivity);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                           // updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+}
